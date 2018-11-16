5b06f0891226bab46897c490f7ff19f0a947f978
Tal Lubko <tallubko@yahoo.com>
2018-11-16 03:26:36 +0200

    adding -D_LARGE_FILE_SOURCE directive

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
+CFLAGS+=-D_FILE_OFFSET_BITS=64
 .PHONY: all
 all:
-	$(CC) -m32 test_fopen_big_file.c -o test_fopen_big_file
+	$(CC) $(CFLAGS) test_fopen_big_file.c -o test_fopen_big_file
 
 .PHONY: clean
 clean:
--- test_fopen_big_file.c
+++ test_fopen_big_file.c
@@ -5,6 +5,8 @@ int main()
 	FILE *f = NULL;
 	const char *fname = "big_file";
 
+	printf("sizeof(off_t) is %d \n", sizeof(off_t));
+
 	if(NULL == (f = fopen(fname, "rb")))
 	{
 		perror("failed to open big_file");
